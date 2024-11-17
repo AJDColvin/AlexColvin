67aff4d20f76d1957428599231e1a15190f9159c
AJDColvin <ajdc21@bath.ac.uk>
2024-11-17 23:03:37 +0000

    Added JS

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function showAbout() {
+    document.getElementById("about").style.display = "block";
+    document.getElementById("projects").style.display = "none";
+}
+
+function showProjects() {
+    document.getElementById("projects").style.display = "block";
+    document.getElementById("about").style.display = "none";
+
+}
