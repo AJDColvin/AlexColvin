0a9c0220863c169f6dc0c507b99e525b5b4c6197
AJDColvin <ajdc21@bath.ac.uk>
2024-11-19 11:21:27 +0000

    Click to surprise bot

--- scripts.js
+++ scripts.js
@@ -8,3 +8,15 @@ function showProjects() {
     document.getElementById("about").style.display = "none";
 
 }
+
+function surpriseBot() {
+    document.getElementById("robot-gif").style.display = "none";
+    document.getElementById("robot-surprise").style.display = "block";
+    setTimeout(returnBotGif, 250);
+}
+
+function returnBotGif() {
+    document.getElementById("robot-gif").style.display = "block";
+    document.getElementById("robot-surprise").style.display = "none";
+
+}
