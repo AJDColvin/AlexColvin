e622b1ae4c880fd4ee838bd4c736708e74a65653
AJDColvin <ajdc21@bath.ac.uk>
2025-12-03 21:34:30 +0000

    Added scrollable projects segement

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
-function showAbout() {
+function showAbout(element) {
     document.getElementById("about").style.display = "block";
     document.getElementById("projects").style.display = "none";
+    if (element) moveUnderline(element);
 }
 
-function showProjects() {
+function showProjects(element) {
     document.getElementById("projects").style.display = "block";
     document.getElementById("about").style.display = "none";
-
+    if (element) moveUnderline(element);
 }
 
 // --------- Robot Gif ----------
@@ -21,3 +22,17 @@ function returnBotGif() {
     document.getElementById("robot-surprise").style.display = "none";
 
 }
+// --------- Gliding Underline ----------
+function moveUnderline(element) {
+    const underline = document.getElementById("menu-underline");
+    underline.style.width = element.offsetWidth + "px";
+    underline.style.left = element.offsetLeft + "px";
+}
+
+// Initialize underline position
+window.onload = function() {
+    const aboutLink = document.querySelector('.links-text'); // Assuming About is the first one
+    if (aboutLink) {
+        moveUnderline(aboutLink);
+    }
+};
